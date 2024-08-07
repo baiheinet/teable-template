1225c61753929fc811249c70568d7591f906fd47
ke <baiheinet@gmail.com>
2024-08-07 05:49:45 +0300

    Update api.ts

--- src/app/t/[slug]/api.ts
+++ src/app/t/[slug]/api.ts
@@ -4,7 +4,7 @@ export const getTemplate = async (id: string) => {
   const tableId = process.env.TABLE_ID;
   const token = process.env.AUTH_TOKEN;
   // get a specific record by id
-  const url = `https://app.teable.io/api/table/${tableId}/record/${id}?fieldKeyType=id`;
+  const url = `https://t.seg.plus/api/table/${tableId}/record/${id}?fieldKeyType=id`;
 
   const response = await fetch(url, {
     headers: {
