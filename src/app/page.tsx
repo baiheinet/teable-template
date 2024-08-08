ac01b823f26c6ace917da92c452a1e028d5b22db
baiheinet <32563583@qq.com>
2024-08-08 04:08:42 +0300

    fix title undefined

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default async function Home() {
                   {item.description}
                 </p>
                 <p className="my-3 text-sm text-gray-500">
-                  `by ${item.creator.title}`
+                  `by ${item.creator?.title || "赛鸽"}`
                 </p>
               </div>
             </Link>
